83109a9f89c053ad6d5dd3a750aee2e72dff2d1e
MaximGuljaev <maximuspo4ta@gmail.com>
2023-04-13 23:24:38 +0300

    add scopes in viewmodelmodule

--- app/src/main/java/android/maxim/retrofitpracticeusername/di/MainActivityViewModelModule.kt
+++ app/src/main/java/android/maxim/retrofitpracticeusername/di/MainActivityViewModelModule.kt
@@ -6,13 +6,16 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ViewModelComponent::class)
 class MainActivityViewModelModule {
 
+    @ViewModelScoped
     @Provides
     fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
@@ -21,6 +24,7 @@ class MainActivityViewModelModule {
             .build()
     }
 
+    @ViewModelScoped
     @Provides
     fun provideLiveData(): MutableLiveData<String> {
         return MutableLiveData()
